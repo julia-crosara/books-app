42626600f597989016288b1013963cfd903c99fb
Julia Crosara <juliacrosara@gmail.com>
2020-01-21 12:54:36 -0800

    add props

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -5,21 +5,22 @@ class SearchBooks extends Component {
   render() {
     return (
       <div className="search-books">
+      
         <div className="search-books-bar">
-          <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+          <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>
+          Close</button>
           <div className="search-books-input-wrapper">
             <input type="text" placeholder="Search by title or author"/>
           </div>
         </div>
-      <div className="search-books-results">
-        <ol className="books-grid"></ol>
+
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+
       </div>
-    </div>
     )
-
   }
-
-
 }
 
 export default SearchBooks
