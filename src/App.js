73c9bf6b62e4ba32f3a06b7a9833296ccfd4a9b4
Julia Crosara <47763769+juliacrosara@users.noreply.github.com>
2020-01-21 20:30:28 -0800

    work on search page specs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import ListBooks from './components/ListBooks'
-// import SearchBooks from './components/SearchBooks'
+import SearchBooks from './components/SearchBooks'
 import * as BooksAPI from './utils/BooksAPI'
 import './App.css'
 
@@ -14,22 +15,31 @@ class BooksApp extends React.Component {
     BooksAPI.getAll().then((books) => {
         this.setState({ books })
     })
-}
+  }
 
   moveShelf = (book, shelf) => {
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
-  })
+    })
     BooksAPI.update(book, shelf)
-}
+  }
 
   render() {
     return (
       <div className="app">
-        <ListBooks
-          books={this.state.books}
-          moveShelf={this.moveShelf}
-         />
+        <Route exact path="/" render={() => (
+          <ListBooks
+            books={this.state.books}
+            moveShelf={this.moveShelf}
+          /> 
+        )} />
+        <Route exact path="/search" render={() => (
+          <SearchBooks
+            moveShelf={this.moveShelf}
+            books={this.props.books}
+          /> 
+        )} />
+
       </div>
     )
   }
