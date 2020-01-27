62a9d9ea7de746bb8b1b9cec374fc06f13977bf3
Julia Crosara <47763769+juliacrosara@users.noreply.github.com>
2020-01-27 12:17:48 -0800

    format

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -10,14 +10,12 @@ class SearchBooks extends Component {
     query: '',
     searchedBooks: []
   }
-
   updateQuery = (query) => {
     this.setState({
       query: query
     })
     this.updateSearchedBooks(query)
   }
-
   updateSearchedBooks = (query) => {
     if (query) {
       BooksAPI.search(query).then((searchedBooks) => {
@@ -31,28 +29,23 @@ class SearchBooks extends Component {
       this.setState({ searchedBooks: [] })
     }
   }
-
   render() {
     return (
       <div className="search-books">
-
         <div className="search-books-bar">
           <Link
             to="/" 
             className="close-search"
           ><button className="close-search">Close</button></Link>
           <div className="search-books-input-wrapper">
-
             <input 
               type="text" 
               placeholder="Search by title or author"
               value={this.state.query}
               onChange={(event) => this.updateQuery(event.target.value)}
             />
-
           </div>
         </div>
-
         <div className="search-books-results">
           <ol className="books-grid">
             {
@@ -69,7 +62,6 @@ class SearchBooks extends Component {
             }
           </ol>
         </div>
-
       </div>
     )
   }
